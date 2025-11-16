9d2446426de0f8df903ad7d54049727ae2f87b7a
casperjdev <casperj.dev@gmail.com>
2025-11-16 15:59:24 +0100

    card component

---
+++ app/components/ui/Card.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+type Props = {
+	additionalClasses?: string;
+};
+
+const { additionalClasses } = defineProps<Props>();
+</script>
+
+<template>
+	<div
+		:class="[
+			'p-4 border border-neutral-900 pattern-[45deg] text-neutral-950/50 relative rounded-md',
+			'before:absolute before:-top-px before:left-1/2 before:-translate-x-1/2 before:w-1/2 before:h-px before:bg-linear-90 before:from-0% before:from-neutral-950 before:via-50% before:via-neutral-50 before:to-100% before:to-neutral-950',
+			'after:absolute after:-bottom-px after:left-1/2 after:-translate-x-1/2 after:w-1/2 after:h-px after:bg-linear-90 after:from-0% after:from-neutral-950 after:via-50% after:via-neutral-50 after:to-100% after:to-neutral-950',
+			additionalClasses,
+		]"></div>
+</template>
